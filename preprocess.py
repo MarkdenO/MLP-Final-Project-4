684cf406040b61478482c87df7263cc5565b109e
Mark <markdenouden9@gmail.com>
2025-03-23 12:21:23 +0100

    changed emojis to <emoji>

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import os
 import spacy
+import re
 nlp = spacy.load("en_core_web_sm")
 
 from collections import defaultdict
@@ -58,6 +59,38 @@ def get_named_entities(parsed_sentences):
             print(f"{sent_id}:  No named entities found.")
         print()
 
+
+def preprocess_emojis(sentence_dict):
+    """
+        Identify emojis in the data.
+        Change emojis to <emoji> token.
+    """
+    emojis_re = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # Emoticons 😀-🙏
+        "\U0001F300-\U0001F5FF"  # Misc symbols & pictographs 🌀-🗿
+        "\U0001F680-\U0001F6FF"  # Transport & map 🚀-🛶
+        "\U0001F700-\U0001F77F"  # Alchemical symbols ⚗️-🝿
+        "\U0001F780-\U0001F7FF"  # Geometric shapes 🞀-🟿
+        "\U0001F800-\U0001F8FF"  # Supplemental arrows 🠀-🢿
+        "\U0001F900-\U0001F9FF"  # Supplemental symbols & pictographs 🤀-🧿
+        "\U0001FA00-\U0001FA6F"  # Chess symbols & more 🨀-🩯
+        "\U0001FA70-\U0001FAFF"  # Extended pictographs 🩰-🫿
+        "\U00002702-\U000027B0"  # Dingbats ✂-➰
+        "\U000024C2-\U0001F251"  # Enclosed characters Ⓜ-🉑
+        "]+",
+        flags=re.UNICODE
+    )
+
+    # Iterate over all sentences and change emojis to <emoji> token
+    for file, sentences in sentence_dict.items():
+        for sent_id, sent in sentences.items():
+            sentence = [(emojis_re.sub("<emoji>", word), label) for word, label in sent]
+            sentence_dict[file][sent_id] = sentence
+    
+    return sentence_dict
+
+
 def preprocess_data(data_path):
     """
        Lowercase everything, except named entities.
@@ -87,5 +120,7 @@ def preprocess_data(data_path):
         for sent_id, sent in sentences.items():
             sentence = [(word.lower(), label) if label not in named_entities else (word, label) for word, label in sent]
             sentence_dict[file][sent_id] = sentence
-
+    # Identify emojis
+    # Change emojis to <emoji> token
+    sentence_dict = preprocess_emojis(sentence_dict)
     print(sentence_dict.items())
